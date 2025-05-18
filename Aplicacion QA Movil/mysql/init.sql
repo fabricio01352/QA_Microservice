9ff8737ec9470845e1e0419b422baeecbc7c5019
fabricio1352 <fabricio.aldaco235101@potros.itson.edu.mx>
2025-05-18 00:59:09 -0400

    first commit

---
+++ Aplicacion QA Movil/mysql/init.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS qa;
+USE qa;
+
+-- DROP TABLE IF EXISTS producto;
+
+
+
+
+CREATE TABLE producto(
+	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY UNIQUE,
+    nombre VARCHAR(50) NOT NULL,
+    descripcion VARCHAR(500) DEFAULT 'N/A',
+    defectuoso BOOLEAN DEFAULT 0,
+    fecha VARCHAR(45) NOT NULL
+	
+
+);
